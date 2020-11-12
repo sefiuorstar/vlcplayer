708ffcdb219c2d2889e87bedc4fefa1e4fa523c6
sefiuorstar <39660202+sefiuorstar@users.noreply.github.com>
2020-11-12 15:00:52 +0100

    Create cat.php

---
+++ cat.php
@@ -0,0 +1,137 @@
+[
+      {
+          "channelName": "Bein Sport 1",
+          "channelImg": "https://4.bp.blogspot.com/-L5tcqonCJEI/WryV7yAv8GI/AAAAAAAAaBk/nPnsqNJX64oS72twFhug9UBoHO6AXtHygCLcBGAs/s1600/bein1.png",
+          "url": "https://ustream.to/stream.php?embed&id=antena-1-romanesti"
+      },
+        {
+          "channelName": "Bein Sport 2",
+          "channelImg": "https://2.bp.blogspot.com/-8vZtbyphJoY/WrzHH4IamzI/AAAAAAAAaB8/d1hxk3XNBHIIwXzQrxhpJN0eb4l5HhlnQCLcBGAs/s1600/2-HD.png",
+          "url": "http://unblocked.to/beinsports2?player=1"
+      },
+        {
+          "channelName": "Bein Sport 3",
+          "channelImg": "https://2.bp.blogspot.com/-LK5uO6tts4k/WrzHH6XJ1VI/AAAAAAAAaB4/DzvG3CvEzv0CTWwtYqwXXRCcUWeVwfYtgCLcBGAs/s1600/3-HD.png",
+          "url": "http://unblocked.to/beinsports3?player=1"
+      },
+      {
+          "channelName": "Bein Sport 4",
+          "channelImg": "https://4.bp.blogspot.com/-M6uswGEXJoY/WrzHH8p7QyI/AAAAAAAAaB0/vq1MtGEDIkg-FD1KoWX24V32-rzWXoGZACLcBGAs/s1600/4-HD.png",
+          "url": "http://unblocked.to/beinsports4?player=1"
+      },
+        {
+          "channelName": "Bein Sport 5",
+          "channelImg": "https://3.bp.blogspot.com/-dNth9_Exlx4/WrzHIV-_7nI/AAAAAAAAaCA/gueOyDPUDocqLrkrvZ0aFiKGbj0jfNzzACLcBGAs/s1600/5-HD.png",
+          "url": "http://unblocked.to/beinsports5?player=1"
+      },
+        {
+          "channelName": "Bein Sport 6",
+          "channelImg": "https://4.bp.blogspot.com/-iks-c8q_Z58/WrzHI5xQP5I/AAAAAAAAaCE/HcshcefvUucl0dOMbJ7deYaC__m0P7ozgCLcBGAs/s1600/6-HD.png",
+          "url": "http://unblocked.to/beinsports6?player=1"
+      },
+        {
+          "channelName": "Bein Sport 7",
+          "channelImg": "https://2.bp.blogspot.com/-h-tSLjQ6LsI/WtFC66Y527I/AAAAAAAAaMA/0PUmlKJCdTowLNOEdDn1hDLHmQkjPv3XwCLcBGAs/s1600/7-HD.png",
+          "url": "http://unblocked.to/beinsports7?player=1"
+      },
+        {
+          "channelName": "Bein Sport 8",
+          "channelImg": "https://3.bp.blogspot.com/-OKDfpRO-RUU/WtFDmVlckoI/AAAAAAAAaMQ/Wsqyea5M3y4JoV1qz8wJpfJ0J1cS7GKJQCLcBGAs/s1600/8-HD.png",
+          "url": "http://unblocked.to/beinsports8?player=1"
+      },
+        {
+          "channelName": "Bein Sport 9",
+          "channelImg": "https://4.bp.blogspot.com/-8gRLzhHRJto/WtFDmeFgN3I/AAAAAAAAaMM/bb6choJIxZAA2sZcvceC61SJLaI8A6CdQCLcBGAs/s1600/9-HD.png",
+          "url": "http://unblocked.to/beinsports9?player=1"
+      },
+        {
+          "channelName": "Bein Sport 10",
+          "channelImg": "https://3.bp.blogspot.com/-uWpdjK671qw/WtFDmfwIMqI/AAAAAAAAaMI/rxPvoYt0XloVOzK_dEM0u-56ESoP7TmcwCLcBGAs/s1600/10-HD.png",
+          "url": "http://unblocked.to/beinsports10?player=1"
+      },
+        {
+          "channelName": "Bein Sport 11 EN",
+          "channelImg": "https://4.bp.blogspot.com/-SPoa_IFWTSk/Wujr4i_s9nI/AAAAAAAAaX4/EkFJpCq0zAs-5t9nCb3Wv2hjpzGl4pBowCLcBGAs/s1600/11-HD.png",
+          "url": "http://unblocked.to/beinsports11?player=1"
+      },
+        {
+          "channelName": "Bein Sport 12 EN",
+          "channelImg": "https://2.bp.blogspot.com/-aS-g82we_xA/Wujr4TCMHtI/AAAAAAAAaX0/gNKIZ_dtAM0jNBRalZgjGMhWNUjTC4PbQCLcBGAs/s1600/12-HD.png",
+          "url": "http://unblocked.to/beinsports12?player=1"
+      },
+        {
+          "channelName": "Bein Sport 1 FR",
+          "channelImg": "https://4.bp.blogspot.com/-OLDGiJizR4c/Wvevm1Pl4zI/AAAAAAAAajU/R6udmdnBFPwXUR6y-IJFG0xVfb_f20ZWQCPcBGAYYCw/s1600/1-FR.png",
+          "url": "http://unblocked.to/beinsports1france?player=1"
+      },
+        {
+          "channelName": "Bein Sport 2 FR",
+          "channelImg": "https://4.bp.blogspot.com/-WfaNuLa6gQ0/Wvevm82j5KI/AAAAAAAAajc/_JSTnpUGGTs0niROGv40giijHro_ZyyyQCPcBGAYYCw/s1600/2-FR.png",
+          "url": "http://unblocked.to/beinsports2france?player=1"
+      },
+        {
+          "channelName": "Bein Sport 3 FR",
+          "channelImg": "https://1.bp.blogspot.com/-7jYByF3Fk70/Wvevm3po2KI/AAAAAAAAajY/qJMUkQ8q6VcpSQjW-kIl8cox1BccAeZdgCPcBGAYYCw/s1600/3-FR.png",
+          "url": "http://unblocked.to/beinsports3france?player=1"
+      },
+        {
+          "channelName": "Bein Sport NEWS",
+          "channelImg": "https://2.bp.blogspot.com/-1fmaOMHjqaM/WtFJLQxGAkI/AAAAAAAAaMs/CsCeRYEXdSE72LkaRiHyKyMgdOJmisr8gCLcBGAs/s1600/Sports_News_ar.png",
+          "url": "http://unblocked.to/beinsportsnews?player=1"
+      },
+        {
+          "channelName": "ابو ظبي الرياضية 1",
+          "channelImg": "https://2.bp.blogspot.com/-dJTtKt2_JOE/W2GmWOM9u-I/AAAAAAAAMzo/soPZdmpFLCUV4JGlI1Wkqv4vyxuV70aqwCLcBGAs/s230-/%25D8%25A7%25D8%25A8%25D9%2588%2B%25D8%25B8%25D8%25A8%25D9%258A%2B%25D8%25A7%25D9%2584%25D8%25B1%25D9%258A%25D8%25A7%25D8%25B6%25D9%258A%25D8%25A9%2B1.png",
+          "url": "https://adtv.ercdn.net/adsport1/adsport1_720p.m3u8"
+      },
+        {
+          "channelName": "ابو ظبي الرياضية 2",
+          "channelImg": "https://4.bp.blogspot.com/-VRrwTjklfq8/W2GtQIT60eI/AAAAAAAAMz0/1EqY3ADIavo3gcaM4BAJMRRMQgs4mXukwCLcBGAs/s230-/%25D8%25A7%25D8%25A8%25D9%2588%2B%25D8%25B8%25D8%25A8%25D9%258A%2B%25D8%25A7%25D9%2584%25D8%25B1%25D9%258A%25D8%25A7%25D8%25B6%25D9%258A%25D8%25A9%2B2.png",
+          "url": "https://adtv.ercdn.net/adsport2/adsport2_720p.m3u8"
+      },
+        {
+          "channelName": "ابو ظبي الرياضية 3",
+          "channelImg": "https://1.bp.blogspot.com/-vM1gtJVr5i0/W2Gv7_Smo6I/AAAAAAAAM0A/q5wLC2vjPEwasWvU7ME8tAIAliXrwFwiACLcBGAs/s230-/%25D8%25A7%25D8%25A8%25D9%2588%2B%25D8%25B8%25D8%25A8%25D9%258A%2B%25D8%25A7%25D9%2584%25D8%25B1%25D9%258A%25D8%25A7%25D8%25B6%25D9%258A%25D8%25A9%2B3.png",
+          "url": "https://tvae.ercdn.net/adsport3/adsport3_720p.m3u8"
+      },
+        {
+          "channelName": "ابو ظبي الرياضية 4",
+          "channelImg": "https://3.bp.blogspot.com/-TMyiU60F5og/W2GxGRt3ZBI/AAAAAAAAM0M/MESOvVjICGAi3_YQjDjL_3MfD2qyESdbwCLcBGAs/s230-/%25D8%25A7%25D8%25A8%25D9%2588%2B%25D8%25B8%25D8%25A8%25D9%258A%2B%25D8%25A7%25D9%2584%25D8%25B1%25D9%258A%25D8%25A7%25D8%25B6%25D9%258A%25D8%25A9%2B4.png",
+          "url": "https://tvae.ercdn.net/adsport4/adsport4_720p.m3u8"
+      },
+        {
+          "channelName": "ابو ظبي الرياضية 5",
+          "channelImg": "https://4.bp.blogspot.com/-mNFF9a_vY-Y/W2G1MIgatPI/AAAAAAAAM0Y/NtGnjuJewOg-zDqMdsLihmGMGxuxz2vSQCLcBGAs/s230-/%25D8%25A7%25D8%25A8%25D9%2588%2B%25D8%25B8%25D8%25A8%25D9%258A%2B%25D8%25A7%25D9%2584%25D8%25B1%25D9%258A%25D8%25A7%25D8%25B6%25D9%258A%25D8%25A9%2B5.png",
+          "url": "https://tvae.ercdn.net/adsport5/adsport5_720p.m3u8"
+      },
+      {
+        "channelName": "ابو ظبي الرياضية 6",
+        "channelImg": "https://4.bp.blogspot.com/-NCU_oYjBTBM/W2G3lSL8prI/AAAAAAAAM0k/_Pt8z7S1GsYFfJskdea252Jpfg20A6b0wCLcBGAs/s230-/%25D8%25A7%25D8%25A8%25D9%2588%2B%25D8%25B8%25D8%25A8%25D9%258A%2B%25D8%25A7%25D9%2584%25D8%25B1%25D9%258A%25D8%25A7%25D8%25B6%25D9%258A%25D8%25A9%2B6.png",
+        "url": "https://tvae.ercdn.net/adsport6/adsport6_720p.m3u8"
+    },
+    {
+      "channelName": "الكأس 1",
+      "channelImg": "http://www.yalla-shoot.com/tv/uploads/data/503c3c788f6fa854e733a530e5e24980b3.png",
+      "url": "https://alkasmsl4.akamaized.net/hls/live/2003123/alkassone1/master.m3u8"
+    },
+    {
+      "channelName": "الكأس 2",
+      "channelImg": "http://www.yalla-shoot.com/tv/uploads/data/544330dd95a99ed10186b9aec9dbbe4d2c.png",
+      "url": "https://alkasmsl4.akamaized.net/hls/live/2003123/alkasstwo/master.m3u8"
+    },
+    {
+      "channelName": "الكأس 4",
+      "channelImg": "http://www.yalla-shoot.com/tv/uploads/data/106c577eea1e797b4ca6ac146181412a438.png",
+      "url": "https://alkasslive-alkassstream-euwe.streaming.media.azure.net/658bc04b-31b6-4051-a55b-62cb771fb21a/7fa1d000-f9ae-42a9-b36e-c46b571de761.ism/QualityLevels(3000000)/Manifest(video,format=m3u8-aapl-v3,audiotrack=audio_1).m3u8"
+    },
+    {
+      "channelName": "الكأس 5",
+      "channelImg": "http://www.yalla-shoot.com/tv/uploads/data/1128507d77dd7e034562e9b859d72b3d1fc.jpg",
+      "url": "https://alkasmsl4.akamaized.net/hls/live/2003123/five/master.m3u8"
+    },
+    {
+      "channelName": "الكأس اون لاين",
+      "channelImg": "http://imageshack.com/a/img35/9531/hcgv.jpg",
+      "url": "https://alkasmsl4.akamaized.net/hls/live/2003123/alkassOnlineZI/master.m3u8"
+    }
+      ]
